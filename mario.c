1b0b2dafc48c9e2a47636c9f406ea60d58da9098
onur <63200764+d3vh0n0r@users.noreply.github.com>
2020-09-22 11:17:57 +0300

    mario.c güncelleme
    buglar giderildi, emniyetli bir şekilde çalışılacak vaziyete getirildi.

--- mario.c
+++ mario.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <cs50.h>
 
-//definition taking height size by prompt
+//definition taking height value by prompt
 int get_hash_number(){
+
     int hash_number = 0;
-
-    printf("enter height: ");
     
-    hash_number = get_int("");
-
+    hash_number = get_int("enter height: ");
+    
     return hash_number;
+    
 }
 
-//definition of making block with hash system
+//definition of block system
 void make_block(int hash_number){
+
     int hash_query = 1;
+
     for(int i = 0; i < hash_number; i++){
-       
        
         for(int j = hash_number - hash_query; j > 0; j--){
             printf(" ");
         }
         
-
         for(int z = 0; z < hash_query; z++){
             printf("#");
         }
@@ -37,13 +37,21 @@ void make_block(int hash_number){
 }
 
 int main(void){
-    int hash_number = 0;
 
-    do{
-        hash_number = get_hash_number();
+    //program will keep ask till get the right value by user   
+    while(true){
+        //calling get_hash_number func to get value
+        int hash_number = get_hash_number();
+
+        if(hash_number >= 1 && hash_number <= 8)
+        {
+            //calling make_block func
+            make_block(hash_number);
+
+            //we got the right answer so we kill the program
+            break;
+        }
     }
-    while(hash_number  < 1 || hash_number > 8);
+    
 
-    make_block(hash_number);
 }
-
