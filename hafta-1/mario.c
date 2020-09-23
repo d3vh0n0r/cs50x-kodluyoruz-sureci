ff8703413565679c4a5dbcebdee93827f5b4b88e
onur <63200764+d3vh0n0r@users.noreply.github.com>
2020-09-23 12:59:55 +0300

    Create mario.c

---
+++ hafta-1/mario.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <cs50.h>
+
+/*
+EXAMPLE OUTPUT
+Note: program allows between 1 and 8
+
+enter the height: 15
+enter the height: sdgdsg
+enter the height: 
+enter the height: 3
+  #
+ ##
+###
+*/
+
+
+//definition of get height value by prompt
+int get_hash_number(){
+
+    int hash_number = 0;
+    
+    hash_number = get_int("enter height: ");
+    
+    return hash_number;
+    
+}
+
+//definition of block system
+void make_block(int hash_number){
+
+    int hash_query = 1;
+
+    for(int i = 0; i < hash_number; i++){
+       
+        for(int j = hash_number - hash_query; j > 0; j--){
+            printf(" ");
+        }
+        
+        for(int z = 0; z < hash_query; z++){
+            printf("#");
+        }
+
+        if(hash_query < hash_number){
+            hash_query++;
+        }
+
+        printf("\n");
+
+    }
+}
+
+int main(void){
+
+    //program will keep ask for height till get the right value by user   
+    while(true){
+        //calling get_hash_number func to get value
+        int hash_number = get_hash_number();
+
+        if(hash_number >= 1 && hash_number <= 8)
+        {
+            //calling make_block func
+            make_block(hash_number);
+
+            //we got the proper answer so we kill the program
+            break;
+        }
+    }
+    
+
+}
